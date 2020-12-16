21f56db9c6f68ee321d65d2d62d34ef720f7596f
Darina K <40147658+HollisLynch@users.noreply.github.com>
2020-12-16 11:40:56 +0100

    fix

--- main.py
+++ main.py
@@ -1,62 +1,91 @@
 from Crypto.Cipher import DES, Blowfish, AES
 from Crypto import Random
-
+import os
 
 def blowfish(plaintext):
     iv = Random.new().read(Blowfish.block_size)
 
-    key = b'abcdefghijklmnop'
+    print("1 - encypt\n2 - decrypt")
+    choice = int(input())
 
-    blowfish_encrypt = Blowfish.new(key, Blowfish.MODE_CFB, iv)
-    blowfish_decrypt = Blowfish.new(key, Blowfish.MODE_CFB, iv)
-
-    encrypted = blowfish_encrypt.encrypt(plaintext)
-    decrypted = blowfish_decrypt.decrypt(encrypted)
-
-    print(encrypted)
-    print(decrypted)
-    return encrypted
-
+    if choice == 1:
+        print("key: ")
+        key = str(input())
+        blowfish_encrypt = Blowfish.new(key, Blowfish.MODE_CFB, iv)
+        blowfish_decrypt = Blowfish.new(key, Blowfish.MODE_CFB, iv)
+        encrypted = blowfish_encrypt.encrypt(plaintext)
+        print(f"encrypted: {encrypted}")
+        print("Decrypt?")
+        i = str(input())
+        if i == "yes":
+            decrypted = blowfish_decrypt.decrypt(encrypted)
+            print(decrypted)
+        return encrypted
+    if choice == 2:
+        print("key: ")
+        key = str(input())
+        blowfish_decrypt = Blowfish.new(key, Blowfish.MODE_CFB, iv)
+        decrypted = blowfish_decrypt.decrypt(plaintext)
+        print(f"decrypted: {decrypted}")
+        return decrypted
 
 def aes(plaintext):
     iv_AES = Random.new().read(AES.block_size)
 
-    key_AES = b'abcdefghijklmnop'
+    print("1 - encypt\n2 - decrypt")
+    choice = int(input())
 
-    aes_encrypt = AES.new(key_AES, AES.MODE_CFB, iv_AES)
-    aes_decrypt = AES.new(key_AES, AES.MODE_CFB, iv_AES)
-
-    encrypted = aes_encrypt.encrypt(plaintext)
-    decrypted = aes_decrypt.decrypt(encrypted)
-
-    print(encrypted)
-    print(decrypted)
-    return encrypted
+    if choice == 1:
+        key = b'Sixteen byte key'
+        aes_encrypt = AES.new(key, AES.MODE_CFB, iv_AES)
+        aes_decrypt = AES.new(key, AES.MODE_CFB, iv_AES)
+        encrypted = aes_encrypt.encrypt(plaintext)
+        print(f"encrypted: {encrypted}")
+        print("Decrypt?")
+        i = str(input())
+        if i == "yes":
+            decrypted = aes_decrypt.decrypt(encrypted)
+            print(decrypted)
+        return encrypted
+    if choice == 2:
+        key = b'Sixteen byte key'
+        aes_decrypt = AES.new(key, AES.MODE_CFB, iv_AES)
+        decrypted = aes_decrypt.decrypt(plaintext)
+        print(f"decrypted: {decrypted}")
+        return decrypted
 
 
 def des(plaintext):
-    iv_DES = Random.get_random_bytes(8)
+    iv_DES = Random.new().read(DES.block_size)
 
-    key_DES = b'abcdefgh'
+    print("1 - encypt\n2 - decrypt")
+    choice = int(input())
 
-    des_encrypt = DES.new(key_DES, DES.MODE_CFB, iv_DES)
-    des_decrypt = DES.new(key_DES, DES.MODE_CFB, iv_DES)
+    if choice == 1:
+        key = b'hello123'
+        des_encrypt = DES.new(key, DES.MODE_ECB, iv_DES)
+        des_decrypt = DES.new(key, DES.MODE_ECB, iv_DES)
+        encrypted = des_encrypt.encrypt(plaintext)
+        print(f"encrypted: {encrypted}")
+        print("Decrypt?")
+        i = str(input())
+        if i == "yes":
+            decrypted = des_decrypt.decrypt(encrypted)
+            print(decrypted)
+        return encrypted
+    if choice == 2:
+        key = b'abcdefgh'
+        des_decrypt = DES.new(key, DES.MODE_ECB, iv_DES)
+        decrypted = des_decrypt.decrypt(plaintext)
+        print(f"decrypted: {decrypted}")
+        return decrypted
 
-    encrypted = des_encrypt.encrypt(plaintext)
-    decrypted = des_decrypt.decrypt(encrypted)
 
-    print(encrypted)
-    print(decrypted)
-    return encrypted
-
+print("Choose algorithm:\n1 - aes\n2 - des\n3 - blowfish")
+choose = int(input())
 
 print("plaintext: ")
 plaintext = str(input())
-
-print("Choose algorithm:")
-print("1 - aes\n2 - des\n3 - blowfish")
-
-choose = int(input())
 
 if choose == 1:
     aes(plaintext)
