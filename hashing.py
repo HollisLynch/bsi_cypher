a37570e76dca62f1fb236e23d6f47820cede4134
Darina K <40147658+HollisLynch@users.noreply.github.com>
2021-01-05 14:26:48 +0100

    salting added

--- hashing.py
+++ hashing.py
@@ -1,21 +1,21 @@
 import hashlib
 from binascii import hexlify
-import scrypt, secrets
-
-password = b'not a number'
-salt = secrets.token_bytes(32)
-scrypt_key = scrypt.hash(password, salt, N=16384, r=8, p=1, 32)
-print('Salt: ', salt)
-print('Key: ', scrypt_key)
+import uuid
+import secrets
 
 data = input("Enter message: ")
 data = str.encode(data)
+salt = secrets.token_bytes(32)
 
 sha3_512 = hashlib.sha3_512(data)
 sha3_512_digest = sha3_512.digest()
 sha3_512_hex_digest = sha3_512.hexdigest()
 
+hashed_msg_salt = hashlib.sha512(data + salt).hexdigest()
+
 print('Printing digest output')
 print(sha3_512_digest)
 print('Printing hexadecimal output')
 print(sha3_512_hex_digest)
+print('Printing salt')
+print(hashed_msg_salt)
